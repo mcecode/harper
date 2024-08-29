8358e4801b3743b70071f8fc86485fd193010eff
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2024-08-30 01:05:02 +0800

    feat(vscode-plugin): add restart server command

--- packages/vscode-plugin/src/extension.ts
+++ packages/vscode-plugin/src/extension.ts
@@ -1,50 +1,70 @@
-import {
-	Executable,
-	LanguageClient,
-	LanguageClientOptions,
-	TransportKind
-} from 'vscode-languageclient/node';
+import type { Executable, LanguageClientOptions } from 'vscode-languageclient/node';
+import type { ExtensionContext } from 'vscode';
 
-let client: LanguageClient;
+import { LanguageClient, TransportKind } from 'vscode-languageclient/node';
+import { commands } from 'vscode';
 
-export function activate() {
-	const server: Executable = {
-		command: 'harper-ls',
-		transport: TransportKind.stdio
-	};
+let client: LanguageClient | null = null;
 
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: [
-			{ language: 'html' },
-			{ language: 'markdown' },
-			{ language: 'rust' },
-			{ language: 'typescriptreact' },
-			{ language: 'typescript' },
-			{ language: 'py' },
-			{ language: 'javascript' },
-			{ language: 'javascriptreact' },
-			{ language: 'go' },
-			{ language: 'c' },
-			{ language: 'cpp' },
-			{ language: 'ruby' },
-			{ language: 'swift' },
-			{ language: 'csharp' },
-			{ language: 'toml' },
-			{ language: 'lua' },
-			{ language: 'sh' },
-			{ language: 'java' }
-		]
-	};
+const serverOptions: Executable = {
+	command: 'harper-ls',
+	transport: TransportKind.stdio
+};
+const clientOptions: LanguageClientOptions = {
+	documentSelector: [
+		{ language: 'html' },
+		{ language: 'markdown' },
+		{ language: 'rust' },
+		{ language: 'typescriptreact' },
+		{ language: 'typescript' },
+		{ language: 'py' },
+		{ language: 'javascript' },
+		{ language: 'javascriptreact' },
+		{ language: 'go' },
+		{ language: 'c' },
+		{ language: 'cpp' },
+		{ language: 'ruby' },
+		{ language: 'swift' },
+		{ language: 'csharp' },
+		{ language: 'toml' },
+		{ language: 'lua' },
+		{ language: 'sh' },
+		{ language: 'java' }
+	]
+};
 
-	// Create the language client and start the client.
-	client = new LanguageClient('harper-ls', 'Harper', server, clientOptions);
-
-	client.start();
+export function activate(context: ExtensionContext): void {
+	context.subscriptions.push(commands.registerCommand('harper-ls.restart', startLanguageServer));
+	startLanguageServer();
 }
 
-export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
+async function startLanguageServer(): Promise<void> {
+	if (client) {
+		if (client.diagnostics) {
+			client.diagnostics.clear();
+		}
+
+		try {
+			await client.stop(2000);
+		} catch (error) {
+			// TODO: Log this out to the user
+		}
 	}
+
+	client = new LanguageClient('harper-ls', 'Harper', serverOptions, clientOptions);
+
+	try {
+		await client.start();
+	} catch (error) {
+		// TODO: Log this out to the user
+		client = null;
+	}
+}
+
+export function deactivate(): Thenable<void> | void {
+	if (!client) {
+		return;
+	}
+
 	return client.stop();
 }
